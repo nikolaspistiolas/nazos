bd52b5856f1633a0fae22b200512e3501df79f43
nikolaspistiolas <npistiolas4@gmail.com>
2021-11-25 13:21:41 +0200

    website functionality

---
+++ node_website/frontend/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component }  from 'react';
+import AllStocks from "./components/AllStocks";
+
+function App() {
+
+  return (
+      <div>
+          <AllStocks />
+      </div>
+
+  );
+}
+
+export default App;
